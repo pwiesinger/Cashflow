726456eea06ad8c1b82186c24006f522cfa2bfe7
paul <paul.n.w@icloud.com>
2019-03-10 22:00:23 +0100

    added filemanager

--- app/src/main/java/com/lab73/cashflow/FileManager.java
+++ app/src/main/java/com/lab73/cashflow/FileManager.java
@@ -1,4 +1,61 @@
 package com.lab73.cashflow;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileManager {
+
+    private static final String verwendungszweckFilename = "verwendungszwecke.csv";
+
+    public static List<String> getVerwendungszwecke(Context context) {
+
+        List<String> list = new ArrayList<>();
+
+        try {
+            InputStream is = context.getAssets().open(verwendungszweckFilename);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = br.readLine();
+            while (line != null) {
+                list.add(line);
+                line = br.readLine();
+            }
+            br.close();
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+        try {
+            FileInputStream fis = context.openFileInput(verwendungszweckFilename);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            String line = br.readLine();
+            while(line != null) {
+                list.add(line);
+                line = br.readLine();
+            }
+            br.close();
+        } catch (IOException io) {
+            // the folder does not exist yet.
+        }
+        return list;
+    }
+
+    public static void addEntryToVerwendungszwecke(String verwendungszweck, Context context) {
+        try {
+            FileOutputStream fos = context.openFileOutput(verwendungszweckFilename, Context.MODE_APPEND);
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(fos), true);
+            pw.println(verwendungszweck);
+            pw.close();
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/lab73/cashflow/MainActivity.java
+++ app/src/main/java/com/lab73/cashflow/MainActivity.java
@@ -2,12 +2,39 @@ package com.lab73.cashflow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ok;
+    private Spinner verwendungszwecke;
+    private Spinner ausgabenEinnahmen;
+    private TextView datum;
+    private TextView numberPicker;
+    private GridView gridView;
+    private TextView cash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ok = findViewById(R.id.ok);
+        verwendungszwecke = findViewById(R.id.verwendungszweck);
+        ausgabenEinnahmen = findViewById(R.id.ausgabenEinnahmen);
+        datum = findViewById(R.id.datum);
+        numberPicker = findViewById(R.id.euro);
+        gridView = findViewById(R.id.gridView);
+        cash = findViewById(R.id.cash);
+
+        
     }
 }
