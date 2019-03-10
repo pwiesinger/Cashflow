08736ae19a86d725cefff26384bab98b9e63cc05
paul <paul.n.w@icloud.com>
2019-03-10 19:48:00 +0100

    init

---
+++ app/src/main/java/com/lab73/cashflow/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lab73.cashflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dfjasölfjaskdlö
+    }
+}
